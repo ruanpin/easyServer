9e4e766a00983b03d3aff211568cada7b8e5771e
vincent <oa51360@gmail.com>
2022-09-24 18:53:26 +0800

    adding Mongoose to connect MongoDB in localhost.

--- index.js
+++ index.js
@@ -1,10 +1,52 @@
 let express = require("express")
 // let multer  = require('multer')
+const mongoose = require('mongoose');
+// import mongoose from 'mongoose';
 
 
 
 const app = express()
 // let upload = multer()
+
+//使用mongoose連線本機mongoDB資料庫
+mongoose.connect('mongodb://localhost:27017/exampleDB')
+    .then(()=>{
+        console.log('connected to mongoDB.')
+    })
+    .catch((err)=>{
+        console.log('connection failed.')
+        console.log(err)
+    });
+
+//資料定義期限制
+const exampleSchema = new mongoose.Schema({
+    name:String,
+    age:Number
+})    
+
+//為資料定義限制
+const Example = mongoose.model("Example", exampleSchema)
+
+//定義資料
+// const John = new Example({
+//     name:'John',
+//     age : 26
+// })
+
+// 加入新資料
+// John.save()
+//     .then(()=>{
+//         console.log('has been saved')
+//     })
+//     .catch((e)=>{
+//         console.log('error save')
+//         console.log(e)
+//     })
+
+//查找資料
+Example.find({}).then((data)=>{
+    console.log(data)
+})
 
 app.use(express.urlencoded({ extended: true }))//收Form data 用 (POST)
 
